f49d104c184bb35e35e8c34da714d7fac9bf2e0f
ManeVoskanyan <manevoskanyan7mm@gmail.com>
2024-01-25 00:05:14 +0400

    imported Google map

--- app/src/main/java/com/example/lives_project/MainActivity.java
+++ app/src/main/java/com/example/lives_project/MainActivity.java
@@ -6,13 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap myMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_google);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        myMap.addMarker(new MarkerOptions().position(sydney).title("Sydney"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
     public void onLessonsClick ( View v ) {
         Intent intent = new Intent(MainActivity.this, LessonsActivity.class);
         startActivity(intent);
