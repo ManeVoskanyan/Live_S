726843f86aa9f371e86a4e83c363db19533119f8
ManeVoskanyan <manevoskanyan7mm@gmail.com>
2024-01-24 17:19:41 +0400

    the initial main activity, lessons activity and the first activity

--- app/src/main/java/com/example/lives_project/LessonsActivity.java
+++ app/src/main/java/com/example/lives_project/LessonsActivity.java
@@ -2,6 +2,7 @@ package com.example.lives_project;
 ;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -14,7 +15,13 @@ public class LessonsActivity extends AppCompatActivity {
         BottomMenuFragment bottomMenuFragment = new BottomMenuFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.lessons_fragment_container, bottomMenuFragment);
+        transaction.add(R.id.lessons_fragment1_container, bottomMenuFragment);
         transaction.commit();
+
+        FragmentManager fragmentManager2 = getSupportFragmentManager();
+        LessonsMenuFragment lessonsMenuFragment = new LessonsMenuFragment();
+        FragmentTransaction transaction2 = fragmentManager2.beginTransaction();
+        transaction2.add(R.id.lessons_fragment2_container, lessonsMenuFragment);
+        transaction2.commit();
     }
 }
---
+++ app/src/main/java/com/example/lives_project/LessonsMenuFragment.java
@@ -0,0 +1,17 @@
+package com.example.lives_project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LessonsMenuFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_lessons_menu, container, false);
+    }
+}
